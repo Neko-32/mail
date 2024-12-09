e5ebadc9c6125001433fa0656d6e6377a34df641
Neko-32 <112032@savs.hcc.edu.tw>
2024-12-09 16:19:27 +0000

    Update

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ fi
 
 # 移動 PiDoorGmail 目錄
 if [ -d "$SCRIPT_DIR/PiDoorGmail" ]; then
-    echo "正在移動 PiDoorGmail 到 $TARGET_DIR..."
+    echo "正在複製 PiDoorGmail 到 $TARGET_DIR..."
     sudo cp "$SCRIPT_DIR/PiDoorGmail" "$TARGET_DIR"
 else
     echo "警告：PiDoorGmail 目錄未找到！"

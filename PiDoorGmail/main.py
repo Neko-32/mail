aee48061985dffa881b2d6dbe96f2139daa9b996
Neko-32 <112032@savs.hcc.edu.tw>
2024-12-18 13:55:17 +0800

    Update main.py

--- PiDoorGmail/main.py
+++ PiDoorGmail/main.py
@@ -99,7 +99,7 @@ def get_gmail_service():
                         creds = flow.run_local_server(
                             port=port,
                             open_browser=False,
-                            authorization_prompt_message='請使用 Ctrl + 滑鼠左鍵開啟連結： {url}'
+                            authorization_prompt_message='請在瀏覽器開啟此連結： {url}'
                         )
                         break
                     except ValueError:
